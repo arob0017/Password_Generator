6d297ed5ed8c5dd46546ccb20ac56ad48f912a6d
Amelia Roberts <arob0017@student.monash.edu>
2020-08-26 08:49:48 +1000

    prompts and confirms for criteria are on click

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = 8<128;
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var lowerSplit = lowercase.split(" ");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperSplit = uppercase.split(" ");
+var number = "0123456789";
+var numberSplit = number.split(" ");
+var special = "!@#$%^&*(){}[]=<>/,.|~?";
+var specialSplit = special.split(" ");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 }
-
-var length = 8 < 128;
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "01234567890";
-var special = "!@#$%^&*(){}[]=<>/,.|~?";
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Criteria prompts and confirm alerts will pop up when the 'generate password' button is clicked
+
+generateBtn.addEventListener("click", writePassword); {
+  function generatePassword(){
+    var length = prompt ("How many characters would you like in your password? (8-128)");
+    
+    if (length <= 8 || length >= 128) {
+      alert ("Password needs to be between 8 and 128 characters. Please try again :-)")
+    } 
+      
+    else {
+      var lowercase = confirm ("Would you like lowercase letters in your password?");
+  
+      var uppercase = confirm ("Would you like UPPERCASE letters in your password?");
+  
+      var numbers = confirm ("Would you like numbers in your password?");
+  
+      var special = confirm ("Would you like special characters (!$&) in your password?");
+
+      if (!lowercase && !uppercase && !numbers && !special) {
+        alert("You must choose atleast one criteria :-)");
+      }
+    }  
+  }
+}
+if (true) {
+var lowerrandom = lowercase[Math.floor(Math.random() * lowercase.length)];
+var upperrandom = uppercase[Math.floor(Math.random() * uppercase.length)];
+var numberrandom = number[Math.floor(Math.random() * number.length)];
+var specialrandom = special[Math.floor(Math.random() * special.length)];
+}
+console.log (lowerrandom, upperrandom, numberrandom, specialrandom);
+
+
+
+writePassword;
 
 
 
 
-var length = prompt ("How many characters would you like in your password? (8-128)");
-alert(length);
-console.log(length.prompt);
 
-var lowercase = confirm ("Would you like lowercase letters in your password?");
-alert(lowercase);
-
-var uppercase = confirm ("Would you like UPPERCASE letters in your password?");
-alert(uppercase);
-
-var numbers = confirm ("Would you like numbers in your password?");
-alert(numbers);
-
-var special = confirm ("Would you like special characters (!$&) in your password?");
-alert(special);
 
